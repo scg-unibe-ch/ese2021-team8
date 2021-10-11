07590b35b0d38d903522523013a7f7310b5eb4f3
Observer-95 <74295534+Observer-95@users.noreply.github.com>
2021-10-11 18:24:23 +0200

    Added User-check in user.service.ts
    Added a boolean method doesUserExist to check whether a username and email is already in the database. Used the method in register-method before creating a user. Added a few comments in user.service.ts

--- ese2021-project-scaffolding/scaffolding/backend/src/services/user.service.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/services/user.service.ts
@@ -4,12 +4,22 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 export class UserService {
-
+    /**
+     * Registers a new user to the database. The UserAttributes are filled in on the frontend.
+     * Before creating the new user, the database is checked whether a user of the same name
+     * or mail already exists.
+     * @param user: The user data from the register form
+     * @return creates either a new user in the database or reject the user
+     * due to failure or existing user of the same name.
+     */
     public register(user: UserAttributes): Promise<UserAttributes> {
         const saltRounds = 12;
-        user.password = bcrypt.hashSync(user.password, saltRounds); // hashes the password, never store passwords as plaintext
-        return User.create(user).then(inserted => Promise.resolve(inserted)).catch(err => Promise.reject(err));
-    }
+        if (this.doesUserExist(user)) {
+            return Promise.reject({ message: 'User already exists'});
+        } else {
+            user.password = bcrypt.hashSync(user.password, saltRounds); // hashes the password, never store passwords as plaintext
+            return User.create(user).then(inserted => Promise.resolve(inserted)).catch(err => Promise.reject(err));
+        }}
 
     public login(loginRequestee: LoginRequest): Promise<User | LoginResponse> {
         const secret = process.env.JWT_SECRET;
@@ -30,11 +40,22 @@ export class UserService {
     }
 
     /**
+     * Searches the database if a name or mail is already registered.
+     * @param user: the inputs from the register form
+     * @return true, if the username or email already exists in the database.
+     * False, if not.
+     */
+    private doesUserExist(user: UserAttributes) {
+        const name = User.findOne({where: {userName: user.userName}});
+        const mail = User.findOne({where: {email: user.email}});
+        return (name || mail) !== null;
+    }
+    /**
      * Searches the database for a user with the fitting username or email.
      * Returns the user if found, or null if not.
-     * @param loginRequestee the request from the frontend
+     * @param loginRequestee: the request from the frontend
      */
-    public findUser(loginRequestee: LoginRequest) {
+    private findUser(loginRequestee: LoginRequest) {
         const user = User.findOne({
             where: {
                 userName: loginRequestee.userName
