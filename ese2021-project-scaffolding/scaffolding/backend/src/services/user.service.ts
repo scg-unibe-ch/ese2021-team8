319b99577a861902a355834e733f38ba78bafc39
Observer-95 <74295534+Observer-95@users.noreply.github.com>
2021-10-14 17:35:42 +0200

    Reworked register-functionality
    The register feature now is working as intended. It checks the database whether a name or email is already taken. Added comments to various functions in user.service.ts

--- ese2021-project-scaffolding/scaffolding/backend/src/services/user.service.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/services/user.service.ts
@@ -2,30 +2,36 @@ import { UserAttributes, User } from '../models/user.model';
 import { LoginResponse, LoginRequest } from '../models/login.model';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import {where} from 'sequelize';
 
 export class UserService {
     /**
      * Registers a new user to the database. The UserAttributes are filled in on the frontend.
      * Before creating the new user, the database is checked whether a user of the same name
-     * or mail already exists.
+     * or mail already exists. If so, the promise gets rejected, else the user is created.
      * @param user: The user data from the register form
      * @return creates either a new user in the database or reject the user
      * due to failure or existing user of the same name.
      */
-    public register(user: UserAttributes): Promise<UserAttributes> {
+    public async register(user: UserAttributes): Promise<UserAttributes> {
         const saltRounds = 12;
-        if (this.doesUserExist(user)) {
-            return Promise.reject({ message: 'User already exists'});
+        const existingName = await this.doesNameExist(user);
+        const existingMail = await this.doesMailExist(user);
+        return Promise.all([existingName, existingMail]).then((existingUser) => {
+        if (existingUser[0] !== null) {
+            return Promise.reject({message: 'Username already taken'});
+        }
+        if (existingUser[1] !== null) {
+            return Promise.reject({message: 'Email already in use'});
         } else {
             user.password = bcrypt.hashSync(user.password, saltRounds); // hashes the password, never store passwords as plaintext
             return User.create(user).then(inserted => Promise.resolve(inserted)).catch(err => Promise.reject(err));
-        }}
+        }});
+    }
 
     public login(loginRequestee: LoginRequest): Promise<User | LoginResponse> {
         const secret = process.env.JWT_SECRET;
-        const quester = this.findUser(loginRequestee);
-        return quester
+
+        return this.findUser(loginRequestee)
             .then(user => {
                 if (bcrypt.compareSync(loginRequestee.password, user.password)) {
                     // compares the hash with the password from the login request
@@ -44,21 +50,22 @@ export class UserService {
     }
 
     /**
-     * Searches the database if a name or mail is already registered.
-     * @param user: the inputs from the register form
-     * @return true, if the username or email already exists in the database.
-     * False, if not.
+     * Checks the database whether a given username is already in the database.
+     * Returns a promise containing a User if one is found with the fitting name, or null.
+     * @param name : the userdata with the name you want to check.
+     * @private helper for @see register()
      */
-    private async doesUserExist(user: UserAttributes): Promise<boolean> {
-        const name = await this.doesNameExist(user);
-        const mail = await this.doesMailExist(user);
-        return (name.userName === user.userName || mail.email === user.email);
-    }
     private doesNameExist(name: UserAttributes): Promise<User | null> {
         return User.findOne({where: {userName: name.userName}});
     }
+    /**
+     * Checks the database whether a given email is already in the database.
+     * Returns a promise containing a User if one is found with the fitting email, or null.
+     * @param name : the userdata with the email you want to check.
+     * @private helper for @see register()
+     */
     private doesMailExist(name: UserAttributes): Promise<User | null> {
-        return User.findOne({where: {userName: name.email}});
+        return User.findOne({where: {email: name.email}});
     }
     /**
      * Searches the database for a user with the fitting username or email.
@@ -68,7 +75,8 @@ export class UserService {
     private async findUser(loginRequestee: LoginRequest): Promise<User | null> {
         const userName = await this.findUsername(loginRequestee);
         const userMail = await this.findEmail(loginRequestee);
-        return Promise.all([userName, userMail]).then((user) => {if (user[0] !== null) { return userName; } else { return userMail; }});
+        return Promise.all([userName, userMail]).then((user) => {
+            if (user[0] !== null) { return userName; } else { return userMail; }});
     }
     private findUsername(loginRequestee: LoginRequest): Promise<User | null> {
         return User.findOne({
