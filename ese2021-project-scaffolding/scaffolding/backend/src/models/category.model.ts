739dea84f4dcac535c80ef4ec6979e626cdd24e4
Observer-95 <74295534+Observer-95@users.noreply.github.com>
2021-10-18 15:36:14 +0200

    Added Category-model in backend
    Added a class that will contain the categories. The category-objects are contained within a table called categories. Methods for adding/deleting objects are still work in progress.

---
+++ ese2021-project-scaffolding/scaffolding/backend/src/models/category.model.ts
@@ -0,0 +1,32 @@
+import { Optional, Model, DataTypes, Sequelize, Association } from 'sequelize';
+
+export interface CategoryAttributes {
+    categoryId: number;
+    categoryName: string;
+}
+
+export interface CategoryCreationAttributes extends Optional<CategoryAttributes, 'categoryId'> {}
+
+export class Category extends Model<CategoryAttributes, CategoryCreationAttributes> implements CategoryAttributes {
+    categoryId!: number;
+    categoryName!: string;
+
+    public static initialize(sequelize: Sequelize) {
+        Category.init({
+            categoryId: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            categoryName: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        },
+            {
+                sequelize,
+                tableName: 'categories'
+            }
+            );
+    }
+}
