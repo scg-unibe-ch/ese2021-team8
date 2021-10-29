f7e356e72b5b1f0178efdc9af76320738cb9f054
MuStAgEDelta <ra.fe@live.de>
2021-10-29 18:13:19 +0200

    Einfügen des Buttons um ein Bild einzufügen, dessweiteren gedanken gemacht über den Datentyp, da String noch ein Platz halter ist. BSp File?
    Schwarzer Rand müsste man vom Button noch entfernen, man könnte auch den normalen grauen button nehmen.

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/post-template/post-template.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/post-template/post-template.component.ts
@@ -23,6 +23,8 @@ export class PostTemplateComponent implements OnInit {
   postTitle : string = '';
   postCategory: Category = this.emptyCategory;
   postContent: string = '';
+  postPicture: string = '';
+  //selectedFile = null;
 
   constructor(
     public httpClient: HttpClient,
@@ -54,12 +56,13 @@ export class PostTemplateComponent implements OnInit {
     this.httpClient.post(environment.endpointURL + "post", {
       title: this.postTitle,
       content: this.postContent,
+      image: this.postPicture,
       creatorId: this.userService.getUser().userId,
       categoryId: this.postCategory.categoryId,
       date: new Date(),
       votes: 0
     }).subscribe((post : any)=>{
-      this.posts.unshift(new Post(post.postId, post.title, post.categoryId, post.content, post.creatorId, post.date, post.votes, ''));
+      this.posts.unshift(new Post(post.postId, post.title, post.categoryId, post.content, post.creatorId, post.date, post.votes, post.image));
       console.log(this.posts);
     });
   }
@@ -67,7 +70,7 @@ export class PostTemplateComponent implements OnInit {
   getPosts(): void{
     this.httpClient.get(environment.endpointURL + "post").subscribe((posts: any)=>{
       posts.forEach((post:any)=>{
-      this.posts.unshift(new Post(post.postId, post.title, post.categoryId, post.content, post.creatorId, post.date, post.votes, ''));})
+      this.posts.unshift(new Post(post.postId, post.title, post.categoryId, post.content, post.creatorId, post.date, post.votes, post.image));})
       this.postTitle = this.postContent = '';
       this.displayPostTemplate = false;
     });
@@ -78,7 +81,28 @@ export class PostTemplateComponent implements OnInit {
       votes: post.votes
     }).subscribe();
   }
+ selectFile({event}: { event: any }){
+
+ }
+  /**
+   *Versuch das Bild aufzunehmen und am Backend zu senden. Jedoch ist Der Daten Typ im Model noch String
+   * und möchte dies nicht einfach ändern
+
+
+  selectFile({event}: { event: any }) {
+
+    // @ts-ignore
+    this.selectedFile() = event.target.files[0];
+
+  }
+  fileUpload() {
+    const fileDate= new FormData();
+    fileDate.append('image', this.selectedFile, this.selectFile.name);
+
+  }
+*/
   /*
+
   createCategory(){
     this.httpClient.post(environment.endpointURL + "category", {
       categoryName: this.emptyCategory.categoryName
