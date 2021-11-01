30068fb9b9f79bcfe39f2d9850a46ad2194fdd9f
Ramona Christen <ramona.christen@students.unibe.ch>
2021-11-01 15:21:52 +0100

    try adding constraints to upload button and get category by id from backend

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/post-template/post/post.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/post-template/post/post.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
 import {Post} from "../../models/post.model";
 import {TodoList} from "../../models/todo-list.model";
+import {UserService} from "../../services/user.service";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-post',
@@ -9,25 +12,48 @@ import {TodoList} from "../../models/todo-list.model";
 })
 export class PostComponent implements OnInit {
 
-
+  voted: boolean = false;
+  whoLiked: string[] = [];
+  currentUser: string = "";
   @Output()
   update = new EventEmitter<Post>();
 
   @Input()
   post: Post = new Post(0,'',0,'',0,new Date(),0, '');
 
-  constructor() { }
+  constructor(
+    public httpClient: HttpClient,
+    public userService: UserService
+  ) {
+    this.currentUser = userService.getUser().username;
+    this.voted = this.whoLiked.includes(this.currentUser);
+  }
   ngOnInit(): void {
   }
 
   //TODO: add constraints so user can only upvote once
    upvote() {
     this.post.votes++;
-    this.update.emit(this.post)
+    this.whoLiked.push(this.userService.getUser().username);
+     this.voted = this.whoLiked.includes(this.currentUser);
+     this.update.emit(this.post);
   }
 
   downvote() {
+    console.log(this.currentUser);
+    console.log(this.whoLiked);
     this.post.votes--;
+    this.whoLiked.push(this.userService.getUser().username);
+    this.voted = this.whoLiked.includes(this.currentUser);
     this.update.emit(this.post)
+  }
+
+  getCategoryName(): string{
+     this.httpClient.get(environment.endpointURL + "category/get/" + this.post.categoryId).subscribe(
+      ((category:any) =>{
+        return category.categoryName;
+      })
+    );
+    return "unknown category";
   }
 }
