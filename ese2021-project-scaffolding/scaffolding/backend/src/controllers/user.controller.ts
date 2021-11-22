b294b6ddbe20d43f3906cb548980902d7913fd6a
Observer-95 <74295534+Observer-95@users.noreply.github.com>
2021-11-22 13:31:13 +0100

    Added update-function to user in backend
    Added a function to update data from a user in the database.

--- ese2021-project-scaffolding/scaffolding/backend/src/controllers/user.controller.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/controllers/user.controller.ts
@@ -2,6 +2,7 @@
 import express, { Router, Request, Response } from 'express';
 import { UserService } from '../services/user.service';
 import { verifyToken } from '../middlewares/checkAuth';
+import { User } from '../models/user.model';
 
 const userController: Router = express.Router();
 const userService = new UserService();
@@ -15,6 +16,21 @@ userController.post('/register',
 userController.post('/login',
     (req: Request, res: Response) => {
         userService.login(req.body).then(login => res.send(login)).catch(err => res.status(500).send(err));
+    }
+);
+
+userController.put('/:id', verifyToken, (req: Request, res: Response) => {
+        User.findByPk(req.params.id)
+            .then(found => {
+                if (found != null) {
+                    found.update(req.body).then(updated => {
+                        res.status(200).send(updated);
+                    });
+                } else {
+                    res.sendStatus(404);
+                }
+            })
+            .catch(err => res.status(500).send(err));
     }
 );
 
